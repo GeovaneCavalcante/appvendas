83ffcf0f9a77c8a2a92bee59547613090719d366
Geovane Cavalcante <geovanefeitosacavalcante@gmail.com>
2018-06-06 15:17:40 -0300

    Finalização de pedidos

--- api_vendas/apps/pedidos/serializers.py
+++ api_vendas/apps/pedidos/serializers.py
@@ -2,6 +2,23 @@ from rest_framework import serializers
 
 from .models import Pedidos
 from apps.produtos.models import Produtos
+from apps.client.models import Client
+from apps.core.models import Status
+
+
+
+class ClientSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Client
+        fields = '__all__'
+
+
+class StatusSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Status
+        fields = '__all__'
 
 
 class ProdutosSerializer(serializers.ModelSerializer):
@@ -14,6 +31,8 @@ class ProdutosSerializer(serializers.ModelSerializer):
 class PedidosSerializer(serializers.ModelSerializer):
     
     produtos = ProdutosSerializer(required=False, many=True)
+    status = StatusSerializer(required=False)
+    client = ClientSerializer(required=False)
 
     class Meta:
         model = Pedidos
